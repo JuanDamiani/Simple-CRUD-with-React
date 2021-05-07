22a072385df4fa1d4ddae6896c5d2055a9149690
JuanDamiani <78281912+JuanDamiani@users.noreply.github.com>
2021-05-06 22:31:23 -0300

    Add files via upload

---
+++ src/Libros/AltaLibro.jsx
@@ -0,0 +1,10 @@
+import '../App.css';
+import React from 'react';
+export default function FormularioLibro() {
+
+    /*Este componente debe devolver un formulario html con todos los campos de la tabla libro del back
+y hacer una petición post a la ruta especificada en el back*/
+    
+const element = <h1>Formulario para ingresar un libro</h1>;
+return element;
+}
---
+++ src/Libros/ListadoLibros.jsx
@@ -0,0 +1,13 @@
+import '../App.css';
+import React from 'react';
+
+ /*Este componente debe hacer una hacer unapeticion GET a la ruta especificada en el back y devolver un listado de todos los libros en la tabla libros
+    Se tiene que poder borrar o modificar datos
+    Debe mostrar que persona tiene cada libro*/
+    
+export default function ListadoGeneros() {
+
+    
+const element = <h1>Listado de géneros</h1>;
+return element;
+}
