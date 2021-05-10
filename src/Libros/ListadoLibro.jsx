61c54c0dfe963b7104b7b02371a69989f36d8267
Juan fenoy <juanpablo@idysol.com>
2021-05-09 21:44:24 -0300

    msj error y menu

--- src/Libros/ListadoLibro.jsx
+++ src/Libros/ListadoLibro.jsx
@@ -36,7 +36,7 @@ export default function ListadoLibro() {
             if (e.message === 'Network Error') {
                 toast.error("No me pude conectar con el servidor");
             } else {
-                toast.error(e.message);
+                toast.error(e.response.data.message);
             }
         }
     }
