1a00a3921c266d18a31e44bd3329f1bc0e9ff07a
Juan fenoy <juanpablo@idysol.com>
2021-05-09 22:34:25 -0300

    1

--- src/Personas/ListadoPersona.jsx
+++ src/Personas/ListadoPersona.jsx
@@ -30,9 +30,12 @@ export default function ListadoPersona() {
         try {
             await axios.delete('http://localhost:3000/api/persona/' + idPersonaABorrar.toString());
             toast.success("Realizado!")
+            
             traerPersonas();
         } catch(e) {
             toast.error(e.response.data.message)
+           
+           
         }
     }
 
@@ -71,7 +74,7 @@ export default function ListadoPersona() {
                     </tbody>
                 </table>
             </div>
-        
+            <Link to="/" >Home</Link>
         </div>
     )
                     }
